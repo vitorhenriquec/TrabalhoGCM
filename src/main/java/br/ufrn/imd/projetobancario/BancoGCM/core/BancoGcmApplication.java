7fd9b2b4e55025cd187d482d2d181d14c4cd8782
Aroldo <aroldo.felix@esig.com.br>
2020-10-12 10:20:29 -0300

    Inicializando Spring com banco de dados

---
+++ src/main/java/br/ufrn/imd/projetobancario/BancoGCM/core/BancoGcmApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.projetobancario.BancoGCM.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoGcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoGcmApplication.class, args);
+	}
+
+}
