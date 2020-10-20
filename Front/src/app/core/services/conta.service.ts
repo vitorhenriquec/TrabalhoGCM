77e18f99987ea9b719cf5778225ef0ebd1f1405a
Aroldo Felix <junioraroldo37@gmail.com>
2020-10-20 15:07:08 -0300

    Criando model e service de conta

---
+++ Front/src/app/core/services/conta.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Conta } from "../models/conta.model";
+
+@Injectable()
+export class ContaService {
+  private url = "/core/v1/conta";
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  findAll(): Observable<Conta[]> {
+    return this.http.get<Conta[]>(this.url);
+  }
+
+  findOne(id: number): Observable<Conta> {
+    return this.http.get<Conta>(`${this.url}/${id}`);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+
+  save(conta: Conta): Observable<number> {
+    return this.http.post<number>(this.url, conta);
+  }
+}
