bc87100455f2f8ebaabf4ad85fb0786ab577409a
Maria Rayane Alves <33555205+mrayanealves@users.noreply.github.com>
2020-10-21 21:49:29 -0300

    Update environment.ts

--- Front/src/environments/environment.ts
+++ Front/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  coreApiUrl: "http://banco-gcm-core.com.br",
+  coreApiUrl: "https://banco-gcm-core.com.br",
   production: false
 };
 
